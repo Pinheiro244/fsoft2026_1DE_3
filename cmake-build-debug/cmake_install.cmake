db436d8bbfea5d3bbb2aaf8dcf2122d32a143851
dxinatau pinheiro <1241358@isep.ipp.pt>
2026-04-23 00:04:44 +0100

    Documentation feita

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/35196/CLionProjects/FirstProject
+# Install script for directory: C:/Users/leand/CLionProjects/FirstProject
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/firstproject")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/FirstProject")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/35196/CLionProjects/FirstProject/cmake-build-debug/install_local_manifest.txt"
+  file(WRITE "C:/Users/leand/CLionProjects/FirstProject/cmake-build-debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/35196/CLionProjects/FirstProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/leand/CLionProjects/FirstProject/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- sources/FirstProject.cpp
+++
@@ -1,8 +0,0 @@
-nge the source le FirstProject.cpp with the following code
-#include <iostream>
-#include "pl5.h"
-int main(void) {
-CounterUi counter;
-counter.run();
-return 0;
-}
